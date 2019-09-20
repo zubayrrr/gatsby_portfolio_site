3549adad70a06ad58e1db4692f144648b4e03b35
zubayrrr <abdulfaiz076@gmail.com>
2019-09-20 14:27:51 +0530

    blog responsive

--- src/components/Globals/BlogPost.js
+++ src/components/Globals/BlogPost.js
@@ -10,19 +10,21 @@ const BlogPost = ({ data }) => {
     <Layout>
       <div className="container">
         <div className="row my-5 justify-content-md-center">
-          <img
-            className="blogpost_img"
-            src={post.jetpack_featured_media_url}
-            alt={post.title}
-          />
-          <h1
-            dangerouslySetInnerHTML={{ __html: post.title }}
-            className="mt-5"
-          />
-          <div
-            dangerouslySetInnerHTML={{ __html: post.content }}
-            className="text-center my-5 blogpost_content"
-          />
+          <div className="col-lg-8 col-md-10 col-sm-6 text-center blogpost">
+            <img
+              className="blogpost_img"
+              src={post.jetpack_featured_media_url}
+              alt={post.title}
+            />
+            <h1
+              dangerouslySetInnerHTML={{ __html: post.title }}
+              className="mt-5"
+            />
+            <div
+              dangerouslySetInnerHTML={{ __html: post.content }}
+              className="text-center my-5 blogpost_content"
+            />
+          </div>
         </div>
       </div>
     </Layout>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -23,36 +23,44 @@ export default class blog extends Component {
       <Layout>
         <SEO title="Blog" />
         <Container id="top">
-          <Row className="mb-5">
-            <div className="col-lg-8 col-md-10 mx-auto ">
-              {this.state.posts
-                .slice(0)
-                .reverse()
-                .map(post => {
-                  return (
-                    <div className="post-preview my-5">
-                      <a href={post.slug} className="blog_title">
-                        <img
-                          src={post.jetpack_featured_media_url}
-                          alt={post.title}
-                        />
-                        <h1 className="post-title">{post.title}</h1>
-                        <h4
-                          className="post-subtitle"
+          {/* <Row className="mb-5"> */}
+          <div>
+            {this.state.posts
+              .slice(0)
+              .reverse()
+              .map(post => {
+                return (
+                  <div className="row align-items-center blog  p-5 my-3">
+                    <div className="col-lg-5 col-md-12 col-sm-12 mx-auto blog-1">
+                      <img
+                        style={{
+                          width: "100%",
+                          height: "auto",
+                        }}
+                        src={post.jetpack_featured_media_url}
+                        alt={post.title}
+                      />
+                    </div>
+                    <div className="col-lg-7 col-md-12 col-sm-12  mx-auto blog-2">
+                      <a href={post.slug} className="blog-link">
+                        <h1 className="blog-title">{post.title}</h1>
+                        <p className="blog-meta" className="text-secondary">
+                          Posted on &nbsp;
+                          {new Date(post.date).toDateString()}
+                        </p>
+                        <span
+                          className="blog-subtitle"
                           dangerouslySetInnerHTML={{
                             __html: post.excerpt,
                           }}
                         />
                       </a>
-                      <p className="post-meta" className="text-secondary">
-                        Posted on &nbsp;
-                        {new Date(post.date).toDateString()}
-                      </p>
                     </div>
-                  )
-                })}
-            </div>
-          </Row>
+                  </div>
+                )
+              })}
+          </div>
+          {/* </Row> */}
         </Container>
       </Layout>
     )
